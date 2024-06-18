6ccc547ecb78d5e7b4b0e86bf7153003ba95071a
Yusuke Sakurai <kerokerokerop@gmail.com>
2024-06-18 14:46:43 +0900

    a

--- src/setup.ts
+++ src/setup.ts
@@ -96,7 +96,6 @@ export async function parseChecksum(
       const [hash, filename] = line.split("  ");
       return [filename, hash];
     });
-  console.log(entries);
   return new Map(entries);
 }
 
